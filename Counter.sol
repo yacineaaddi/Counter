3adc3c09e3e9ca748d45509c40bbd54abfc034c7
Souhail EL <121339594+yacineaaddi@users.noreply.github.com>
2023-03-18 20:05:24 +0100

    Create Counter.sol

---
+++ Counter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Counter {
+
+    uint public number;
+
+   
+
+    function increment() external {
+
+        number += 1;
+
+    }
+
+}
