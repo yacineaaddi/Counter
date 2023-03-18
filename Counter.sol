9d8f2b6a702e8cb9b958e16dca27611cb9d6cf62
Souhail EL <121339594+yacineaaddi@users.noreply.github.com>
2023-03-18 20:11:46 +0100

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -1,16 +1,43 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+// SPDX-License-Identifier: GPL-3.0
 
-contract Counter {
+pragma solidity 0.8.17;
 
-    uint public number;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-   
+contract Firsttoken is  ERC20Capped, Ownable{
 
-    function increment() external {
 
-        number += 1;
+    /*uint256 public blockreward = (10) * (10 ** uint(decimals()));*/
 
+    constructor () ERC20 ( "FaucetToken" , "FTS" ) ERC20Capped ( 10000000000000000 * 10** uint(decimals())) {
+    
+        _mint(payable(msg.sender), 100000000 * 10 ** uint(decimals()));
+        
     }
 
+    /*function _MintMinerReward() internal {
+        _mint(block.coinbase, blockreward);}*/
+
+
+    function _MintToken(uint256 amount) public {
+        _mint(payable(msg.sender), amount * 10 ** uint(decimals()));
+    }
+
+
+    function _beforeTokenTransfer(address from , address to , uint256 value) internal virtual override{
+        if(from != address(0) && to != block.coinbase && block.coinbase != address(0)){
+        /*_MintMinerReward();*/
+        }
+        super._beforeTokenTransfer(from , to , value);
+     }
+
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped) {
+        require(ERC20.totalSupply() + amount <= cap(), "You could not mint more");
+        super._mint(account, amount);
+    }
+
+    
 }
